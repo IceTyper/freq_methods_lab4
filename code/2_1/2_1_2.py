282dab1b168d4218a51e5cb8db715bd6db34631e
IceTyper <175852686+IceTyper@users.noreply.github.com>
2026-06-03 19:01:42 +0300

    убрал из 1 задания все графики, где 'c' не равно нулю. Много выпилил я конечно...

--- code/2_1/2_1_2.py
+++ code/2_1/2_1_2.py
@@ -33,7 +33,7 @@ def create_and_save_plot(filename, data):
     plt.xlim(t_axis[0], t_axis[-1])
     plt.tight_layout()
 
-    save_dir = r"C:\Users\fmusa\ITMOStudies\freq_methods\freq_methods_lab4\images\2_1_2"
+    save_dir = r"images\2_1_2"
     os.makedirs(save_dir, exist_ok=True)
     filepath = os.path.join(save_dir, filename)
     plt.savefig(filepath, dpi=300)
